ee8adf817120fc9264b838db3ab7c9b486c95ed6
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-11-13 18:50:28 +0100

    all css done & register Page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -6,6 +6,8 @@ import CartPage from "./pages/Cart/CartPage";
 import LoginPage from "./pages/Login/LoginPage";
 import ProfilePage from "./pages/Profile/ProfilePage";
 import OrderTrackPage from "./pages/OrderTrack/OrderTrackPage";
+import RegisterPage from "./pages/Register/RegisterPage";
+import OrdersPage from "./pages/Orders/OrdersPage";
 
 export default function AppRoutes() {
   return (
@@ -16,8 +18,10 @@ export default function AppRoutes() {
       <Route path="/food/:id" element={<FoodPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/track/:orderId" element={<OrderTrackPage />} />
+      <Route path="/orders/:filter?" element={<OrdersPage />} />
     </Routes>
   );
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,32 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getUser = () =>
-  localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
+  localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user'))
     : null;
 
 export const login = async (email, password) => {
-  const { data } = await axios.post("api/users/login", { email, password });
-  localStorage.setItem("user", JSON.stringify(data));
+  const { data } = await axios.post('api/users/login', { email, password });
+  localStorage.setItem('user', JSON.stringify(data));
   return data;
 };
 
-export const register = async (registerData) => {
-  const { data } = await axios.post("api/users/register", registerData);
-  localStorage.setItem("user", JSON.stringify(data));
+export const register = async registerData => {
+  const { data } = await axios.post('api/users/register', registerData);
+  localStorage.setItem('user', JSON.stringify(data));
   return data;
 };
 
 export const logout = () => {
-  localStorage.removeItem("user");
+  localStorage.removeItem('user');
 };
 
-export const updateProfile = async (user) => {
-  const { data } = await axios.put("/api/users/updateProfile", user);
-  localStorage.setItem("user", JSON.stringify(data));
+export const updateProfile = async user => {
+  const { data } = await axios.put('/api/users/updateProfile', user);
+  localStorage.setItem('user', JSON.stringify(data));
   return data;
 };
 
-export const changePassword = async (passwords) => {
-  await axios.put("/api/users/changePassword", passwords);
+export const changePassword = async passwords => {
+  await axios.put('/api/users/changePassword', passwords);
 };

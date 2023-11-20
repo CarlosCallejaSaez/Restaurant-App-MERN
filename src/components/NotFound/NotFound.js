2438e617daa0ec061f05ea9b9c3169391cfbc9df
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-11-20 20:38:29 +0100

    delay form render.com message

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound({ message, linkRoute, linkText }) {
   return (
     <div className={classes.container}>
-      {message}
+      <p>{message}</p>
       <Link to={linkRoute}>{linkText}</Link>
     </div>
   );
